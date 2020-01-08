f9d22f246226e170d2dc55c618d08c5bfb6bec16
Кирилл Печенко <kirillcomcom5@gmail.com>
2020-01-08 23:31:31 +0300

    1.0

--- KP-lb17/Program.cs
+++ KP-lb17/Program.cs
@@ -3,13 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace KP_lb17
 {
     class Program
     {
+        static int[] Procces(int[] ar)
+        {
+            int[] tmp = new int[ar.Length];
+            int result = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                int x = ar[i];
+                result = Math.Abs(x * x - x * x * x) - ((7 * x) / (x * x * x - 15 * x));
+                tmp[i] = result;
+            }
+            return tmp;
+        }
+        static async void ProccesRqAsync(IPEndPoint ep)
+        {
+            try
+            {
+                using (Socket sc = new Socket(SocketType.Stream, ProtocolType.Tcp))
+                {
+                    sc.Bind(ep);
+                    sc.Listen(10);
+                    Console.WriteLine("Waiting for connection!");
+                    while (true)
+                        using (Socket handler = await sc.AcceptAsync())
+                        {
+                            Console.WriteLine($"Connected:{(handler.LocalEndPoint as IPEndPoint).ToString()}");
+                            Console.WriteLine("Get data:");
+                            using (NetworkStream st = new NetworkStream(handler))
+                            {
+                                var fr = new BinaryFormatter();
+                                var dataSet = fr.Deserialize(st) as int[];
+                                Console.WriteLine("Data:" + string.Join(",", dataSet) + "!");
+                                var proccesed = Procces(dataSet);
+                                Console.WriteLine("Proccesed:" + string.Join(",", proccesed) + "!");
+                                fr.Serialize(st, proccesed);
+                                Console.WriteLine("Done!");
+                                Console.WriteLine("Waiting for connection!");
+                            }
+                        }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
         static void Main(string[] args)
         {
+            IPAddress ip = IPAddress.Loopback;
+            int port = 8080;
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            ProccesRqAsync(endPoint);
+            Console.ReadKey();
         }
     }
 }
--- KP-lb17_Client/Program.cs
+++ KP-lb17_Client/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace KP_lb17_Client
 {
@@ -10,6 +13,31 @@ namespace KP_lb17_Client
     {
         static void Main(string[] args)
         {
+            var r = new Random();
+            int[] array = Enumerable.Range(0, 5).Select(x => r.Next(-10,10)).ToArray();
+            Console.WriteLine("Data:" + string.Join(",", array) + "!");
+            IPAddress ip = IPAddress.Loopback;
+            int port = 8080;
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            try
+            {
+                using (Socket sk = new Socket(SocketType.Stream, ProtocolType.Tcp))
+                {
+                    sk.Connect(endPoint);
+                    var fr = new BinaryFormatter();
+                    using (NetworkStream st = new NetworkStream(sk))
+                    {
+                        fr.Serialize(st, array);
+                        int[] proccesed = fr.Deserialize(st) as int[];
+                        Console.WriteLine("Proccesed:" + string.Join(",", proccesed) + "!");
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
